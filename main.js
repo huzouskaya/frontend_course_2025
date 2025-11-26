d5548e065c35d3943186e7b5991ce1670653d49e
alicewolf_13 <vspace.feature@gmail.com>
2025-11-27 09:40:02 +1000

    old html, css, js files were deleted

--- main.js
+++
@@ -1,8 +0,0 @@
-function changeGreeting() {
-    const greetingElement = document.getElementById('greeting');
-    if (greetingElement.textContent === 'Hello, World!') {
-        greetingElement.textContent = 'Hello, FEFU!';
-    } else {
-        greetingElement.textContent = 'Hello, World!';
-    }
-}
